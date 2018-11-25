876ed9e624498b8ef7ce7cdf49943de6f875df90
thomas <tngo0508@gmail.com>
2018-11-25 01:40:45 -0800

    modify activity to host the fragment dynamically using supportFragmentManager.

--- app/src/main/java/edu/fullerton/cpsc411/mini_app03/MainActivity.kt
+++ app/src/main/java/edu/fullerton/cpsc411/mini_app03/MainActivity.kt
@@ -2,12 +2,6 @@ package edu.fullerton.cpsc411.mini_app03
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
-
-import java.util.Locale.US
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,43 +9,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val defaultListener = object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-
+        if (savedInstanceState == null) {
+            // During initial setup, plug in the details fragment.
+            val details = SimpleFragment().apply {
+                arguments = intent.extras
             }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val notEmpty : TextView.() -> Boolean = { text.isNotEmpty() }
-
-                if (notEmpty(FileSizeInput) and notEmpty(NetworkSpeedInput)) {
-                    val speed= java.lang.Double.valueOf(NetworkSpeedInput.text.toString())
-                    val size = java.lang.Double.valueOf(FileSizeInput.text.toString())
-
-                    if (size == 0.0) Result.text = getString(R.string.invalid_input)
-                    else {
-                        val output = Result.toOneDec(Result.cal(speed, size)) + " seconds"
-                        Result.text = output
-                    }
-                } else {
-                    val time = Result.toOneDec(Result.cal(100.0, 100.0)) + " seconds"
-                    Result.text = time
-                }
-            }
-
+            supportFragmentManager.beginTransaction()
+                    .add(android.R.id.content, details)
+                    .commit()
         }
-        NetworkSpeedInput.addTextChangedListener(defaultListener)
-        FileSizeInput.addTextChangedListener(defaultListener)
-    }
-
-    private fun TextView.cal(networkSpeed: Double?, fileSize: Double?): Double {
-        return networkSpeed!! * Math.pow(2.0, 20.0) * 8.0 / (fileSize!! * Math.pow(10.0, 6.0))
-    }
-
-    private fun TextView.toOneDec(num: Double?): String {
-        return String.format(US, "%.1f", num)
     }
 }
---
+++ app/src/main/java/edu/fullerton/cpsc411/mini_app03/SimpleFragment.kt
@@ -0,0 +1,124 @@
+package edu.fullerton.cpsc411.mini_app03
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_simple.*
+import kotlinx.android.synthetic.main.fragment_simple.view.*
+import java.util.Locale.US
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val NET_WORK = "net_work"
+private const val FILE_SIZE = "file_size"
+private const val RESULT = "result"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [SimpleFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [SimpleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class SimpleFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var netSpeed: Double = 0.0
+    private var fileSize: Double = 0.0
+    private var result: String? = null
+//    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            it.putDouble(NET_WORK, netSpeed)
+            it.putDouble(FILE_SIZE, fileSize)
+            it.putString(RESULT, result)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_simple, container, false)
+        val fileSizeInput = rootView.FileSizeInput
+        val networkSpeedInput = rootView.NetworkSpeedInput
+        val resultView = rootView.Result
+
+        val defaultListener = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val notEmpty: TextView.() -> Boolean = { text.isNotEmpty() }
+
+                if (notEmpty(fileSizeInput) and notEmpty(networkSpeedInput)) {
+                    val speed = java.lang.Double.valueOf(networkSpeedInput.text.toString())
+                    netSpeed = speed
+                    val size = java.lang.Double.valueOf(fileSizeInput.text.toString())
+                    fileSize = size
+
+                    if (size == 0.0) {
+                        resultView.text = getString(R.string.invalid_input)
+                        result = resultView.text.toString()
+                    } else {
+                        val output = resultView.toOneDec(Result.cal(speed, size)) + " seconds"
+                        resultView.text = output
+                        result = output
+                    }
+                } else {
+                    val time = resultView.toOneDec(Result.cal(100.0, 100.0)) + " seconds"
+                    resultView.text = time
+                    result = time
+                }
+            }
+
+        }
+        networkSpeedInput.addTextChangedListener(defaultListener)
+        fileSizeInput.addTextChangedListener(defaultListener)
+
+        return rootView
+    }
+
+    private fun TextView.cal(networkSpeed: Double?, fileSize: Double?): Double {
+        return networkSpeed!! * Math.pow(2.0, 20.0) * 8.0 / (fileSize!! * Math.pow(10.0, 6.0))
+    }
+
+    private fun TextView.toOneDec(num: Double?): String {
+        return String.format(US, "%.1f", num)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param speed Network speed in Mbps
+         * @param size File size in MiB
+         * @param result time in seconds
+         * @return A new instance of fragment SimpleFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(speed: Double, size: Double, result: Double) =
+                SimpleFragment().apply {
+                    arguments = Bundle().apply {
+                        putDouble(NET_WORK, speed)
+                        putDouble(FILE_SIZE, size)
+                        putDouble(RESULT, result)
+                    }
+                }
+    }
+}
