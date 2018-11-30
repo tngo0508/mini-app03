0d3b7a1e8e0482b285ed859caf93105f746c056b
thomas <tngo0508@gmail.com>
2018-11-29 17:35:44 -0800

    clean code

--- app/src/main/java/edu/fullerton/cpsc411/mini_app03/MainActivity.kt
+++ app/src/main/java/edu/fullerton/cpsc411/mini_app03/MainActivity.kt
@@ -1,10 +1,7 @@
 package edu.fullerton.cpsc411.mini_app03
 
-//import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-//import android.support.v4.app.FragmentManager
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import androidx.fragment.app.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val simpleFragment = SimpleFragment()
         if (supportFragmentManager.findFragmentById(R.id.fragment_container) == null) {
-//            supportFragmentManager.beginTransaction()
-//                    .add(R.id.fragment_container, simpleFragment)
-//                    .addToBackStack(null)
-//                    .commit()
             supportFragmentManager.transaction(allowStateLoss = true) {
                 add(R.id.fragment_container, SimpleFragment())
             }
